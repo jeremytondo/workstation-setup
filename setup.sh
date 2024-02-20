a06dd57f7a10045a1fc5f2bf61d57f1fe3cca46e
jeremytondo <jmargari@gmail.com>
2024-02-20 10:11:20 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo curl -sS https://raw.githubusercontent.com/jeremytondo/workstation-setup/main/base-install.sh | bash
 
-git clone --bare https://github.com/jeremytondo/dotfiles.git .dotfiles
+git clone --bare git@github.com:jeremytondo/dotfiles.git .dotfiles
 
 git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout
 
